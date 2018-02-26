b90b047b47b56e066d91b224528b45180d127ec2
GentleSpoon <me.github@gentlespoon.com>
2018-02-26 05:23:21 -0800

    Update checkip.php

--- checkip.php
+++ checkip.php
@@ -7,20 +7,27 @@
  * Time   : 19:35
  */
 
+// User-agent is required, otherwise IP138 will refuse service.
 $options  = array('http' => array('user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'));
 $context  = stream_context_create($options);
+
+// Change this to your own service provider
 $string = 'http://ip138.com/ips138.asp?ip='.$_GET['ip'].'&action=2';
+
 $string = file_get_contents("$string", false, $context);
 
+// Charset conversion
 $string = iconv('GB2312', 'UTF-8', $string);
 
+// Data extraction and formatting
 $string = explode('<li>', $string);
 unset($string[0]);
 foreach ($string as $k => $v) {
   $v = explode('</li>', $v);
   $string[$k] = $v[0];
 }
-
 $string = implode("\n", $string);
+
+
 echo $string;
 
