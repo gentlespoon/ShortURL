56682540e2c29dab7123029f80a63577082ba5c1
GentleSpoon <github@gentlespoon.com>
2019-02-22 22:04:04 -0800

    Advanced user manage with password reset

---
+++ server-express/modules/email.js
@@ -0,0 +1,67 @@
+nodeMailer = require('nodemailer');
+
+class Email {
+
+  constructor() {
+    this.mailer = nodeMailer.createTransport({
+      host: process.env.SMTP_SERVER,
+      port: process.env.SMTP_PORT,
+      secure: true,
+      auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS
+      }
+    });
+  }
+
+  validate(email) {
+    // console.log('checking email format');
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email.toLowerCase());
+  }
+
+
+  send(mail) {
+    mail.html = mail.html.split('{{email}}').join(mail.to);
+    setTimeout( () => {
+      this.mailer.sendMail(mail, (err, info) => {
+        if (err) return console.log('Failed to send mail to ' + mail.to, err, info);
+        console.log('Mail sent to ' + mail.to);
+      });
+    }, 1000);
+  };
+
+
+  // email templates
+  template(t) {
+    var body = `<div style="background-color: #006088; color: white; padding: 1em; font-size: 1.2em; font-weight: 600;">GtSpn.com ShortURL</div>`;
+    switch (t) {
+      case 'register':
+        body += `
+<p>Although I do not understand why you choose my service, but anyway, thanks for registering!</p>
+<p>You may create custom short URLs now.</p>
+<p><a href="https://gtspn.com/" target="_blank" style="
+  text-decoration: none;
+  padding: .5em;
+  border-radius: .25em;
+  color: white;
+  font-weight: 600;
+  box-shadow: 1px 1px 1px black;
+  background-color: #006088;
+">Create a custom Short URL!</a></p>
+`;
+        break;
+      case 'forget':
+        body += `
+<p>People always forget their password. Just follow the link to reset your password.</p>
+<p><a href="https://gtspn.com/user/reset/{{token}}" target="_blank">https://gtspn.com/user/reset/{{token}}</a></p>
+<p>If you did not request to change your password, please disregard this email.<br>Your password is still safe and will not be changed.</p>
+`;
+    }
+    body += `<div style="margin-top: 2em; font-size: 0.8em">This email is intended for {{email}}.</div>`;
+    return body;
+  }
+
+};
+
+module.exports = new Email();
---
+++ server-express/routes/user.js
@@ -0,0 +1,197 @@
+var express = require('express');
+var bcrypt = require('bcryptjs');
+var uuidv4 = require('uuid/v4');
+
+var moment = require('moment');
+var momentFormat = process.env.momentFormat;
+
+var router = express.Router();
+
+var mysql = require('mysql');
+var db_user = mysql.createConnection({
+  host     : process.env.DB_HOST_u,
+  user     : process.env.DB_USER_u,
+  password : process.env.DB_PASS_u,
+  database : process.env.DB_NAME_u
+});
+
+var api = require('../modules/apiFormat');
+var email = require('../modules/email');
+
+router.post('/login', (req, res, next) => {
+  if (req.session.account_id)
+    return res.send(api(1, {account_id: req.session.account_id}));
+  // validate input type
+  if (typeof req.body.email !== 'string' || req.body.email === ''
+   || typeof req.body.password !== 'string' || req.body.password === '')
+    return res.send(api(0, 'Incomplete credentials.'));
+  // get user from database
+  db_user.query("SELECT account_id, password FROM accounts WHERE (login_name=? OR email=?) AND banned_time<?", [req.body.email, req.body.email, moment().toISOString()], (err, result, fields) => {
+    if (err) return res.send(api(0, err));
+    if (result.length) {
+      // if user exists, compare password
+      if (bcrypt.compareSync(req.body.password, result[0].password)) {
+        req.session.account_id = result[0].account_id;
+        db_user.query("INSERT INTO login_history (auth_login, auth_time_iso, remote_address, from_application, result, account_id) VALUES (?, ?, ?, ?, ?, ?)", [
+          req.body.email, moment().toISOString(), req.connection.remoteAddress, process.env.appName, 'success', req.session.account_id]);
+        console.log(req.session);
+        return res.send(api(1, {account_id: req.session.account_id}));
+      } else {
+        // if user does not exist / incorrect password
+        db_user.query("INSERT INTO login_history (auth_login, auth_time_iso, remote_address, from_application, result) VALUES (?, ?, ?, ?, ?)", [
+          req.body.email, moment().toISOString(), req.connection.remoteAddress, process.env.appName, 'fail']);
+        return res.send(api(0, 'Invalid credentials.'));
+      }
+    } else {
+      // if user does not exist / incorrect password
+      db_user.query("INSERT INTO login_history (auth_login, auth_time_iso, remote_address, from_application, result) VALUES (?, ?, ?, ?, ?)", [
+            req.body.email, moment().toISOString(), req.connection.remoteAddress, process.env.appName, 'fail']);
+      return res.send(api(0, 'Email is not registered.'));
+    }
+  });
+});
+
+
+router.post('/registerCheck', (req, res, next) => {
+  // validate input type
+  if (req.body.email) {
+    if (typeof req.body.email !== 'string' || !email.validate(req.body.email))
+      return res.send(api(0, 'Invalid email.'));  
+    db_user.query("SELECT account_id FROM accounts WHERE email=?", [req.body.email], (err, result, fields) => {
+      if (err) return res.send(api(0, err));
+      if (result.length) return res.send(api(0, 'Email is already registered.'));
+      return res.send(api(1));
+    });
+  }
+});
+
+
+router.post('/register', (req, res, next) => {
+  // validate input type
+  if (typeof req.body.email !== 'string' || !email.validate(req.body.email)
+   || typeof req.body.password !== 'string' || req.body.password === ''
+  )
+    return res.send(api(0, 'Incomplete credentials.'));  
+  var account_id = uuidv4();
+  // Check for duplicate username
+  db_user.query("SELECT account_id, login_name FROM accounts WHERE email=? OR account_id=?", [req.body.email, account_id], (err, result, fields) => {
+    if (err) return res.send(api(0, err)); 
+    if (result.length) return res.send(api(0, 'Email is already registered.'));
+    // If username is not taken, check password strength.
+    if (req.body.password.length < 8) 
+      return res.send(api(0, 'Password must not be shorter than 8 characters.'));
+    if (req.body.password.match(/[A-Z]/g) === null)
+      return res.send(api(0, 'Password must contain at least 1 upper case letter.'));
+    if (req.body.password.match(/[a-z]/g) === null)
+      return res.send(api(0, 'Password must contain at least 1 lower case letter.'));
+    if (req.body.password.match(/[0-9]/g) === null)
+      return res.send(api(0, 'Password must contain at least 1 digit.'));
+    // hash password
+    var hash = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    db_user.query("INSERT INTO accounts (account_id, email, password, referrer, register_time) VALUES (?, ?, ?, ? ,?)", [account_id, req.body.email, hash, req.session.account_id, moment().toISOString()], (err, result, fields) => {
+      if (err) return res.send(api(0, err));
+      // schedule a welcome email
+      let mail = {
+        from: 'me@gentlespoon.com',
+        to: req.body.email,
+        subject: 'Welcome to GtSpn.com ShortURL',
+        html: email.template('register')
+      };
+      email.send(mail);
+      db_user.query('SELECT * FROM accounts WHERE account_id=?', [account_id], (err, result, fields) => {
+        if (err) return res.send(api(0, err));
+        if (!result.length) return res.send(api(0, 'Failed to get user from UserCenter'));
+        req.session.account_id = result[0].account_id;
+        return res.send(api(1, {account_id: req.session.account_id}));
+      })
+    });
+  });
+});
+
+
+router.post('/forget', (req, res, next) => {
+  if (typeof req.body.email !== 'string' || req.body.email === '') return res.send(api(0, 'Not enough info for retrieving password.'));
+  db_user.query('SELECT account_id FROM accounts WHERE email=?', [req.body.email], (err, result, fields) => {
+    if (err) return res.send(api(0, err));
+    if (result.length) {
+      let token = uuidv4();
+      let account_id = result[0].account_id;
+      db_user.query('INSERT INTO tokens (purpose, account_id, token, expire) VALUES (?, ?, ?, ?)', ['reset', account_id, token, moment().add(1, 'd').toISOString()], (err, result, fields) => {
+        if (err) return res.send(api(0, err));
+        // schedule a welcome email
+        let mail = {
+          from: 'me@gentlespoon.com',
+          to: req.body.email,
+          subject: 'Reset your password - GtSpn.com ShortURL',
+          html: email.template('forget').split('{{token}}').join(token)
+        };
+        email.send(mail);
+        return res.send(api(1, 'If your email address is in our database, we will send you a link to reset your password.'));
+      })
+    } else {
+      return res.send(api(1, 'If your email address is in our database, we will send you a link to reset your password.'));
+    }
+  })
+});
+
+router.post('/checkToken', (req, res, next) => {
+  if (req.body.token) {
+    db_user.query('SELECT account_id FROM tokens WHERE token=? AND expire>? AND used=0 AND purpose=?', [req.body.token, moment().toISOString(), 'reset'], (err, result, fields) => {
+      if (err) return res.send(api(0, err));
+      if (result.length) {
+        var account_id = result[0].account_id;
+        db_user.query('UPDATE tokens SET used=1 WHERE token=?', [req.body.token], (err, result, fields) => {
+          if (err) res.send(api(0, 'Failed to update token status'));
+          req.session.resetAccount = account_id;
+          return res.send(api(1, 'Valid token'));
+        })
+      } else {
+        req.session.resetAccount = '';
+        return res.send(api(0, 'Invalid token'));
+      }
+    })
+  }
+});
+
+router.post('/reset', (req, res, next) => {
+  // validate input type
+  if (!req.session.resetAccount)
+    return res.send(api(0, 'Which account are you trying to reset?'));
+  if (typeof req.body.password !== 'string' || req.body.password === '')
+    return res.send(api(0, 'Incomplete credentials.'));  
+  // Check password strength.
+  if (req.body.password.length < 8) 
+    return res.send(api(0, 'Password must not be shorter than 8 characters.'));
+  if (req.body.password.match(/[A-Z]/g) === null)
+    return res.send(api(0, 'Password must contain at least 1 upper case letter.'));
+  if (req.body.password.match(/[a-z]/g) === null)
+    return res.send(api(0, 'Password must contain at least 1 lower case letter.'));
+  if (req.body.password.match(/[0-9]/g) === null)
+    return res.send(api(0, 'Password must contain at least 1 digit.'));
+  var hash = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+  db_user.query("UPDATE accounts SET password=? WHERE account_id=?", [hash, req.session.resetAccount], (err, result, fields) => {
+    if (err) return res.send(api(0, 'Failed to reset password.'));
+    return res.send(api(1, 'Password has been reset.'));
+  });
+});
+
+
+
+
+router.all('/logout', (req, res, next) => {
+  req.session.destroy();
+  return res.send(api(1));
+});
+
+
+router.get('/check', (req, res, next) => {
+  console.log(req.session);
+  if (req.session.account_id) {
+    return res.send(api(1, {account_id: req.session.account_id}));
+  } else {
+    return res.send(api(0));
+  }
+})
+
+
+module.exports = router;
