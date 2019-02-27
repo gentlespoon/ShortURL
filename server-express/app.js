95b3133d9f3b06edde0e43c6565495caaabbb320
GentleSpoon <github@gentlespoon.com>
2019-02-26 21:00:12 -0800

    Correct IP for server behind proxy

--- server-express/app.js
+++ server-express/app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var fs = require('fs');
 var expressip = require('express-ip');
 var visitLog = require('./modules/visitLog');
 
