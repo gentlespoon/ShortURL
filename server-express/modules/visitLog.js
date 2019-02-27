13253b70c816573dce0b9cfa12da6ba86e9217c6
GentleSpoon <github@gentlespoon.com>
2019-02-26 21:00:12 -0800

    Correct IP for server behind proxy

--- server-express/modules/visitLog.js
+++ server-express/modules/visitLog.js
@@ -15,7 +15,8 @@ class VisitLog {
     });
     if (req.lookup_url === undefined) req.lookup_url = null;
     if (req.target_url === undefined) req.target_url = null;
-    db.query('INSERT INTO history (request_url, short_url, long_url, request_time, ip, location, user_agent) VALUES (?, ?, ?, ?, ?, ?, ?)', [req.url, req.lookup_url, req.target_url, moment().toISOString(), req.ip, req.location, req.headers["user-agent"]], (err, result, fields) => {
+    var ip = req.ip + req.connection.remoteAddress + req.headers['x-forwarded-for']?req.headers['x-forwarded-for']:'';
+    db.query('INSERT INTO history (request_url, short_url, long_url, request_time, ip, location, user_agent) VALUES (?, ?, ?, ?, ?, ?, ?)', [req.url, req.lookup_url, req.target_url, moment().toISOString(), ip, req.location, req.headers["user-agent"]], (err, result, fields) => {
       if (err) throw err;
     });
   }
